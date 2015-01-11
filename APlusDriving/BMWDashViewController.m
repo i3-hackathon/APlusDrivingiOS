eadff97399a63c979f89c2e863d0861020bb5397
Yash Varma <yashvarma@Yashs-MacBook-Pro-2.local>
2015-01-10 20:37:37 -0800

    updated circle drawing

--- APlusDriving/BMWDashViewController.m
+++ APlusDriving/BMWDashViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "BMWDashViewController.h"
-#import <MagicPieLayer.h>
 #import <SHPieChartView/SHPieChartView.h>
 
 @interface BMWDashViewController ()
@@ -35,39 +34,6 @@
     [concentricPieChart addAngleValue:0.40 andClolor:[UIColor redColor]];
     
     [self.view addSubview:concentricPieChart];
-//    PieLayer * pieLayer = [[PieLayer alloc] init];
-//    pieLayer.frame = CGRectMake(20, 20, 200, 200);
-//    pieLayer.backgroundColor = [UIColor whiteColor].CGColor;
-//    [self.view.layer addSublayer:pieLayer];
-//    
-//    CALayer * circle = [[CALayer alloc] init];
-//    circle.backgroundColor = [UIColor whiteColor].CGColor;
-//    circle.borderColor = [UIColor blackColor].CGColor;
-//    circle.borderWidth = 1.0f;
-//    circle.frame = CGRectMake(30, 30, 180, 180);
-//    circle.cornerRadius = circle.frame.size.width/2;
-//    [self.view.layer addSublayer:circle];
-//    
-//    CALayer * circleOuter = [[CALayer alloc] init];
-//    circleOuter.backgroundColor = [UIColor clearColor].CGColor;
-//    circleOuter.borderColor = [UIColor blackColor].CGColor;
-//    circleOuter.borderWidth = 1.0f;
-//    circleOuter.frame = CGRectMake(20, 20, 200, 200);
-//    circleOuter.cornerRadius = circle.frame.size.width/2;
-//    [self.view.layer addSublayer:circleOuter];
-//    
-//    
-//    
-//    [pieLayer addValues:@[[PieElement pieElementWithValue:10 color:[UIColor greenColor]],
-//                          [PieElement pieElementWithValue:90 color:[UIColor whiteColor]]] animated:NO];
-////                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]],
-//                          [PieElement pieElementWithValue:1 color:[UIColor blueColor]]] animated:NO];
 
 }
 
